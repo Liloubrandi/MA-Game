2e5e9fa4a8e964491906a8562cb06cb7e6ec2004
Lilou.Brandi <lia.brandenberger@gmx.ch>
2023-05-13 13:51:17 +0200

    Füge Sound hinzu (vom Tutorial)

--- demo01_besser.py
+++ demo01_besser.py
@@ -32,8 +32,10 @@ class Player(pygame.sprite.Sprite):
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -5)
+            move_up_sound.play()
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0, 5)
+            move_down_sound.play()
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-5, 0)
         if pressed_keys[K_RIGHT]:
@@ -92,8 +94,22 @@ class Cloud(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+# Setup for sounds. Defaults are good.
+pygame.mixer.init()
 # Initialize pygame
 pygame.init()
+
+# Load and play background music
+# Sound source: http://ccmixter.org/files/Apoxode/59262
+# License: https://creativecommons.org/licenses/by/3.0/
+pygame.mixer.music.load("Apoxode_-_Electric_1.mp3")
+pygame.mixer.music.play(loops=-1)
+
+# Load all sound files
+# Sound sources: Jon Fincher
+move_up_sound = pygame.mixer.Sound("Rising_putter.ogg")
+move_down_sound = pygame.mixer.Sound("Falling_putter.ogg")
+collision_sound = pygame.mixer.Sound("Collision.ogg")
 
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
@@ -168,6 +184,10 @@ while running:
     if pygame.sprite.spritecollideany(player, enemies):
         # If so, then remove the player and stop the loop
         player.kill()
+        # Stop any moving sounds and play the collision sound
+        move_up_sound.stop()
+        move_down_sound.stop()
+        collision_sound.play()
         running = False
 
     # Update the display
@@ -175,3 +195,9 @@ while running:
 
     # Ensure program maintains a rate of 30 frames per second
     clock.tick(30)
+
+# All done! Stop and quit the mixer.
+pygame.mixer.music.stop()
+pygame.mixer.quit()
+
+#Wieso geht das mit den Bildern nur, wenn beide (Dokument und Bild) im MA-Game Ordner selbst drin sind? 
