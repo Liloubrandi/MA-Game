0b33abc8f9413157a67e033b5d571042323892da
Lilou.Brandi <lia.brandenberger@gmx.ch>
2023-05-20 14:58:59 +0200

    Klammere Funktion aus, da sie nicht funktioniert

--- my_game.py
+++ my_game.py
@@ -22,18 +22,19 @@ BLOCK_WIDTH = DISPLAY_WIDTH/9
 BLOCK_HIGHT = DISPLAY_LENGTH/13
 screen = pygame.display.set_mode([DISPLAY_WIDTH, DISPLAY_LENGTH])
 
-def links_oder_rechts():
+'''def links_oder_rechts():
+    global rectangle_x #vergessen, hat deshalb zuerst Fehlemeldung gegeben
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 rectangle_x = rectangle_x + BLOCK_WIDTH
             if event.key == K_LEFT:
-                rectangle_x = rectangle_x - BLOCK_WIDTH 
-    return rectangle_x
+                rectangle_x = rectangle_x - BLOCK_WIDTH
+    return rectangle_x'''
 
 #While-Schlaufe - machen bis running = False
 running = True
-rectangle_y = 250
+rectangle_y = 0
 rectangle_x = 0 + 4 * BLOCK_WIDTH
 while running:
 
@@ -51,17 +52,17 @@ while running:
                 rectangle_x = rectangle_x - BLOCK_WIDTH
 
     rectangle_y = rectangle_y + 0.01
+    #Überprüfe, ob sich der Block verschieben soll
+    #rectangle_x = links_oder_rechts()
 
     #Hintergrund weiss machen
     screen.fill((255, 255, 255))
 
-    #Überprüfe, ob sich der Block verschieben soll
-    #links_oder_rechts()
-
-    # Draw a solid blue circle in the center
+    #Zeichne ein Rechteck oben in die Mitte
     pygame.draw.rect(screen, (255, 0, 0), [rectangle_x, rectangle_y, BLOCK_WIDTH, BLOCK_HIGHT])
 
     # Flip the display - aktualisieren
     pygame.display.flip()
+
 #ganz am Ende
 pygame.quit()
