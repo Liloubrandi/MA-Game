c3803e90ba12067e915bb90d46306df0e03ef26a
Lilou.Brandi <lia.brandenberger@gmx.ch>
2023-08-14 08:26:59 +0200

    Versuche eine Klasse für beide Würfel zusammen zu machen

--- TetraMerge.py
+++ TetraMerge.py
@@ -67,9 +67,29 @@ board = [
                 rectangle_x = rectangle_x - BLOCK_WIDTH
                 list_y = rectangle_x / BLOCK_WIDTH
         #soll der Block nach unten fallen?
-        elif event.type == MOVEBLOCK:
+        elif event.type == BLOCKFALL:
             rectangle_y = rectangle_y + BLOCK_HEIGHT
             list_x = rectangle_y / BLOCK_HEIGHT'''
+
+class Duo(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.add(Block(BLOCK_WIDTH, BLOCK_HEIGHT, rectangle_x))
+        self.add(Block(BLOCK_WIDTH, BLOCK_HEIGHT, rectangle2_x))
+
+    def check_events(events):
+        if not Block.is_falling(block): #wieso reicht hier block? Müsste block_2 nicht auch?
+            return
+        for event in events:
+            if event.type == BLOCKFALL:
+                Block.move(block, 'down')
+            if event.type == KEYDOWN:
+                if event.key == K_RIGHT:
+                    if block.rect.x + BLOCK_WIDTH <= DISPLAY_WIDTH - BLOCK_WIDTH:
+                        Block.move(block, 'right')
+                if event.key == K_LEFT:
+                    if block.rect.x - BLOCK_WIDTH >= 0:
+                        Block.move(block, 'left')
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, width, height, pos_x):
@@ -81,7 +101,23 @@ class Block(pygame.sprite.Sprite):
         self.rect.y = 0
         self.number = random.randint(1, 6)
 
-    def check_events(self, events):
+    def move(self, direction):
+        if direction == 'down':
+            board[self.rect.y//BLOCK_HEIGHT][self.rect.x//BLOCK_WIDTH] = False
+            self.rect.y = self.rect.y + BLOCK_HEIGHT
+        if direction == 'right':
+            board[self.rect.y//BLOCK_HEIGHT][self.rect.x//BLOCK_WIDTH] = False
+            self.rect.x = self.rect.x + BLOCK_WIDTH
+        if direction == 'left':
+            board[self.rect.y//BLOCK_HEIGHT][self.rect.x//BLOCK_WIDTH] = False
+            self.rect.x = self.rect.x - BLOCK_WIDTH
+
+    def is_falling(self):
+        if self.rect.y < DISPLAY_LENGTH - BLOCK_HEIGHT:
+            if board[self.rect.y//BLOCK_HEIGHT+ 1][self.rect.x//BLOCK_WIDTH] == False:
+                return True
+    
+    '''def check_events(self, events):
         if not self.is_falling():
             return
         for event in events:
@@ -101,27 +137,23 @@ class Block(pygame.sprite.Sprite):
                             self.rect.x = self.rect.x - BLOCK_WIDTH
                         #list_y = self.rect.x // BLOCK_WIDTH
             #soll der Block nach unten fallen?
-            elif event.type == MOVEBLOCK:
+            elif event.type == BLOCKFALL:
                 if self.rect.y + BLOCK_HEIGHT <= DISPLAY_LENGTH - BLOCK_HEIGHT:
                     board[self.rect.y//BLOCK_HEIGHT][self.rect.x//BLOCK_WIDTH] = False
                     self.rect.y = self.rect.y + BLOCK_HEIGHT
-                    #list_x = self.rect.y // BLOCK_HEIGHT
-
-    def is_falling(self):
-        if self.rect.y < DISPLAY_LENGTH - BLOCK_HEIGHT:
-            if board[self.rect.y//BLOCK_HEIGHT+ 1][self.rect.x//BLOCK_WIDTH] == False:
-                return True
+                    #list_x = self.rect.y // BLOCK_HEIGHT'''
     
 
 #Kreiere ein eigenes Event, welches jede Sekunde ausgeführt wird -> um den Block nach unten zu bewegen
-MOVEBLOCK = pygame.USEREVENT + 1
-pygame.time.set_timer(MOVEBLOCK, 500)
+BLOCKFALL = pygame.USEREVENT + 1
+pygame.time.set_timer(BLOCKFALL, 500)
 
-block = Block(BLOCK_WIDTH, BLOCK_HEIGHT, rectangle_x)
-block_2 = Block(BLOCK_WIDTH, BLOCK_HEIGHT, rectangle2_x)
+#block = Block(BLOCK_WIDTH, BLOCK_HEIGHT, rectangle_x)
+#block_2 = Block(BLOCK_WIDTH, BLOCK_HEIGHT, rectangle2_x)
+block = Duo()
 
 block_group = pygame.sprite.Group()
-block_group.add(block, block_2)
+block_group.add(block)
 
 #While-Schlaufe - machen bis running = False
 while running:
@@ -136,28 +168,19 @@ while running:
                 running = False
 
     has_active_block = False
+    #for block in block_group:
+        #block.check_events(events)
     for block in block_group:
-        block.check_events(events)
-        if block.is_falling():
-            has_active_block = True
+        Duo.check_events(events)
+    if block.is_falling():
+        has_active_block = True
 
     if not has_active_block:
-        block = Block(BLOCK_WIDTH, BLOCK_HEIGHT, rectangle_x)
-        block_2 = Block(BLOCK_WIDTH, BLOCK_HEIGHT, rectangle2_x)
-        block_group.add(block, block_2)
+        block = Duo()
+        block_group.add(block)
 
     #Hintergrund weiss machen
     screen.fill((255, 255, 255))
-
-    # if rectangle_y > DISPLAY_LENGTH - BLOCK_HEIGHT:
-    #     fest_x = rectangle_x
-    #     rectangle_x = 0 + 4 * BLOCK_WIDTH
-    #     rectangle_y = 0
-    # #Nach oben braucht es nicht, da es ja nie nach oben geht
-    # if rectangle_x > DISPLAY_WIDTH - BLOCK_WIDTH:
-    #     rectangle_x = DISPLAY_WIDTH - BLOCK_WIDTH
-    # if rectangle_x < 0:
-    #     rectangle_x = 0
 
     #Zeichne ein Rechteck oben in die Mitte
     #pygame.draw.rect(screen, (255, 0, 0), [rectangle_x, rectangle_y, BLOCK_WIDTH, BLOCK_HEIGHT])
@@ -167,13 +190,13 @@ while running:
         list_x = block.rect.x // BLOCK_WIDTH
         list_y = block.rect.y // BLOCK_HEIGHT
         if not board[list_y][list_x]:
-            board[list_y][list_x] = block.number
+            board[list_y][list_x] = block
             '''for row in board:
                 print(row)
             print(' ')'''
     
     #block_group.draw(screen)
-    # Flip the display - aktualisieren
+    #Flip the display - aktualisieren
     pygame.display.flip()
 
 #ganz am Ende
